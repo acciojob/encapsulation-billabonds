9aadd17042a406bb6ea7394120990f82d10b416c
Shivam <goelshivam2012@gmail.com>
2023-03-15 11:51:03 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Shivam");
+
+        String ans = obj.getName();
+
+        System.out.println(ans);
+    }
+
 }
